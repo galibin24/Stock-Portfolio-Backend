8659db0685148dbf330fe47fe1c9db8282bd3aca
Nikita Galibin <galibin24@mail.ru>
2020-10-08 22:27:05 +0300

    HTTPS

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM node:12-alpine
 WORKDIR /usr/src/app
 COPY package*.json ./
+COPY ./key.pem ./key.pem
+COPY ./cert.pem ./cert.pem
+
 RUN npm install
 COPY . .
 EXPOSE 3000
--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 var express = require("express");
 var app = express();
 var cors = require("cors");
+const https = require("https");
+
 const axios = require("axios");
 const { response } = require("express");
 
 app.use(cors());
 
 app.get("/api", (req, res, next) => {
-  params = req.query;
-  axios
-    .get(
-      `https://query1.finance.yahoo.com/v8/finance/chart/${params.ticker}?period1=${params.startDate}&period2=${params.endDate}&interval=1d`
-    )
-    .then((response) => res.send(response.data));
+	params = req.query;
+	axios
+		.get(
+			`https://query1.finance.yahoo.com/v8/finance/chart/${params.ticker}?period1=${params.startDate}&period2=${params.endDate}&interval=1d`
+		)
+		.then((response) => res.send(response.data));
 });
 
-app.listen(3000, function () {
-  console.log("CORS-enabled web server listening on port 3000");
-});
+https
+	.createServer(
+		{
+			key: fs.readFileSync("./key.pem"),
+			cert: fs.readFileSync("./cert.pem"),
+		},
+		app
+	)
+	.listen(3000);
